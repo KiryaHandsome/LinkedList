5904d3605126ca5dfb9cf2a4425aa5313849bff3
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-02-06 01:06:20 +0300

    feat: add 'add()' and 'get()' methods; test them

--- src/main/java/com/clevertec/list/MyLinkedList.java
+++ src/main/java/com/clevertec/list/MyLinkedList.java
@@ -1,7 +1,7 @@
 package com.clevertec.list;
 
-public class MyLinkedList<T> {
-    private static class Node<T> {
+public class MyLinkedList<T>  {
+    static class Node<T> {
         private Node<T> next;
         private Node<T> prev;
         private T data;
@@ -10,24 +10,44 @@ public class MyLinkedList<T> {
             this.data = data;
         }
 
-        public Node<T> getNext() {
-            return next;
-        }
-
-        public void setNext(Node<T> next) {
-            this.next = next;
-        }
-
-        public Node<T> getPrev() {
-            return prev;
-        }
-
-        public void setPrev(Node<T> prev) {
-            this.prev = prev;
+        Node() {
         }
     }
 
-    private int length = 0;
+    private int size = 0;
     private Node<T> head;
     private Node<T> tail;
+
+    public MyLinkedList() {
+    }
+
+    public void add(T item) {
+        Node<T> newNode = new Node<>(item);
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        ++size;
+    }
+
+    //TODO: iterate in both directions
+    public T get(int index) {
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<T> current = head;
+        while(index > 0) {
+            current = current.next;
+            --index;
+        }
+        return current.data;
+    }
+
+    public int size() {
+        return size;
+    }
+
 }
--- src/test/java/MyLinkedListTest.java
+++ src/test/java/MyLinkedListTest.java
@@ -2,11 +2,26 @@ import com.clevertec.list.MyLinkedList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+import java.util.List;
+
 public class MyLinkedListTest {
     @Test
     public void createList() {
         MyLinkedList<Integer> list = new MyLinkedList<>();
         Assertions.assertNotNull(list);
+    }
+
+    @Test
+    public void addElementsToEndOfEmptyList() {
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        list.add(123);
+        list.add(228);
+        list.add(337);
+        Assertions.assertEquals(list.get(0), 123);
+        Assertions.assertEquals(list.get(1), 228);
+        Assertions.assertEquals(list.get(2), 337);
     }
 
 }
