668ebc9e0af185a67bd3c38c6031e06d05be1d47
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-02-06 00:18:53 +0300

    feat: add class MyLinkedList and create its basis

---
+++ src/main/java/com/clevertec/list/MyLinkedList.java
@@ -0,0 +1,33 @@
+package com.clevertec.list;
+
+public class MyLinkedList<T> {
+    private static class Node<T> {
+        private Node<T> next;
+        private Node<T> prev;
+        private T data;
+
+        Node(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        public Node<T> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<T> prev) {
+            this.prev = prev;
+        }
+    }
+
+    private int length = 0;
+    private Node<T> head;
+    private Node<T> tail;
+}
